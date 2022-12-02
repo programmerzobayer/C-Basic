be2f262cdf07292e1ff86616d310b1b86bfc735b
programmerzobayer <programmerzobayer@gmail.com>
2022-12-02 12:51:43 +0600

    Upload sizeof.c

---
+++ sizeof.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i;
+    char c ;
+    float f ;
+    double d ;
+    // lu means unassign data types value
+    printf("Integer number storage size %lu bytes \n",sizeof(i));
+    printf("Character number storage size %lu bytes  \n",sizeof(c));
+    printf("Float number storage size %lu bytes  \n",sizeof(f));
+    printf("Double number storage size %lu bytes  \n",sizeof(d));
+
+}
