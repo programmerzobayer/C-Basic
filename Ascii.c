2ceeb4640bb2a852209958e231c328ba8a3628ea
programmerzobayer <programmerzobayer@gmail.com>
2022-12-03 14:21:18 +0600

    Upload Ascii.c

---
+++ Ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int num ;
+    char character;
+    printf("Enter a Integer number :");
+    scanf("%d", &num);
+    printf("ASCII is : %c\n" ,num);no
+    return 0 ;
+}
